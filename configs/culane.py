235d61e2aca2eef98a877c12c3682db3ebf79006
zjf <3220639726@qq.com>
2022-12-21 00:11:05 +0800

    完善culane.py，在路径中添加CULane目录

--- configs/culane.py
+++ configs/culane.py
@@ -28,7 +28,7 @@ shp_loss_w = 0.0
 # EXP
 note = ''
 
-log_path = '/data/ldp/zjf/log'
+log_path = '/data/ldp/zjf/log/CULane'
 
 # FINETUNE or RESUME MODEL PATH
 finetune = None
@@ -36,10 +36,10 @@ resume = None
 
 # TEST and Visualization
 # model_mark = 模型所属的训练/在该训练中所属的批次【即相对log_path的路径】
-model_mark = '/20221219_161349_lr_1e-01_b_32/ep049.pth'
-test_model = '/data/ldp/zjf/log' + model_mark
-test_work_dir = '/data/ldp/zjf/evaluation' + model_mark
-visualize_work_dir = '/data/ldp/zjf/visualization' + model_mark
+model_mark = '/20221219_161349_lr_1e-01_b_32/ep000.pth'
+test_model = '/data/ldp/zjf/log/CULane' + model_mark
+test_work_dir = '/data/ldp/zjf/evaluation/CULane' + model_mark
+visualize_work_dir = '/data/ldp/zjf/visualization/CULane' + model_mark
 
 num_lanes = 4
 
